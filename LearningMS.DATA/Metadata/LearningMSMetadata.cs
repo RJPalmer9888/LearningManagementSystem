e2fbac17261f0a78e13556420d4a79fbbc274af0
Ryan Palmer <rjpalmer9888@gmail.com>
2021-08-09 14:15:14 -0500

    Initial Commit Again

---
+++ LearningMS.DATA/Metadata/LearningMSMetadata.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningMS.DATA/*.Metadata*/
+{
+    #region Course Metadata
+    public class CourseMetadata
+    {
+        //public string CourseId { get; set; }
+
+        [DisplayFormat(NullDisplayText = "[-N/A-]")]
+        [UIHint("MultilineText")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(200, ErrorMessage = "*Value must be 200 characters or less")]
+        [Display(Name = "Course Name")]
+        public string CourseName { get; set; }
+
+        [StringLength(500, ErrorMessage = "*Value must be 500 characters or less")]
+        [Display(Name = "Course Description")]
+        public string CourseDescription { get; set; }
+
+        [StringLength(200, ErrorMessage = "*Value must be 200 characters or less")]
+        [Display(Name = "Course Image")]
+        public string CoursePhoto { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+
+    [MetadataType(typeof(CourseMetadata))]
+    public partial class Course { }
+    #endregion
+}
---
+++ LearningMS.MVC/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace LearningMS.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
